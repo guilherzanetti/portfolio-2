8f0b883e5b419cc5c7fcef17d84e2b803f7c113a
guiz <zanettig520@gmail.com>
2023-02-15 21:22:59 -0300

    projetos-v1

---
+++ script.js
@@ -0,0 +1,12 @@
+// ----------------- Biblioteca ScrollReveal -------------------
+
+window.sr = ScrollReveal({ reset: true });
+
+//banner - ScrollReveal
+ScrollReveal().reveal('.h1-banner',{duration: 1500});
+
+//chamada pra ação - ScrollReveal
+ScrollReveal().reveal('.content2-btn-display',{duration: 2000});
+ScrollReveal().reveal('.img-lottie',{duration: 1000});
+
+// -----------------------------------------------------------
