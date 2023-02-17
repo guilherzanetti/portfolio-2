11862d7fe1f52301c2f49d29ddcf5552651da9b4
guiz <zanettig520@gmail.com>
2023-02-16 22:07:19 -0300

    responsive-version

--- script.js
+++ script.js
@@ -10,3 +10,24 @@ ScrollReveal().reveal('.content2-btn-display',{duration: 2000});
 ScrollReveal().reveal('.img-lottie',{duration: 1000});
 
 // -----------------------------------------------------------
+
+// ---------------------- Menu responsivo
+
+document.querySelector('.list-header');
+document.querySelector('.button-header-r');
+
+var headerButton = document.querySelector('.button-header-r');
+
+var listaDoHeader = document.querySelector('.list-header');
+
+var btnSairHeader = document.querySelector("#btn-sair-header");
+
+// Abrir o Menu Responsivo
+headerButton.addEventListener("click", function(){
+    listaDoHeader.style.display = "block"
+});
+
+// Fechar o Menu Responsivo
+btnSairHeader.addEventListener("click", function(){
+    listaDoHeader.style.display = "none"
+});
